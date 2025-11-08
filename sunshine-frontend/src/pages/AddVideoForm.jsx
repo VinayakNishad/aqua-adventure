b936ea056d1a146e3e9b9afcbd86374a2c9ddb09
VinayakNishad <143430439+VinayakNishad@users.noreply.github.com>
2025-11-08 19:31:23 +0530

    Update API URL to use environment variable

--- sunshine-frontend/src/pages/AddVideoForm.jsx
+++ sunshine-frontend/src/pages/AddVideoForm.jsx
@@ -22,7 +22,7 @@ const AddVideoForm = ({ onVideoAdded }) => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/videos", { link: trimmedLink });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/videos`, { link: trimmedLink });
 
       // Show backend success message
       toast.success(res.data.message);
