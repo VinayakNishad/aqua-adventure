0e35ded20c0f785ee436c7a0b8cdc7d9cafd030a
VinayakNishad <143430439+VinayakNishad@users.noreply.github.com>
2025-11-08 17:46:07 +0530

    Use environment variable for API URL

--- sunshine-frontend/src/pages/EditPackage.jsx
+++ sunshine-frontend/src/pages/EditPackage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API = process.env.REACT_APP_API_URL;
 
 const EditPackage = () => {
   const { id } = useParams();
