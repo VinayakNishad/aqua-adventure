72843dca4e54b211eda1bf305d5f4dc87c3fd1b4
VinayakNishad <143430439+VinayakNishad@users.noreply.github.com>
2025-11-08 17:42:17 +0530

    Update API URL to use environment variable

--- sunshine-frontend/src/components/ActivitiesSection.jsx
+++ sunshine-frontend/src/components/ActivitiesSection.jsx
@@ -6,7 +6,7 @@ import { auth } from "../pages/firebaseconfig";
 import { ToastContainer, toast } from 'react-toastify';
 import { Carousel } from "react-bootstrap";
 
-const API = "http://localhost:5000/api";
+const API = process.env.REACT_APP_API_URL;
 
 // --- SVGs for admin ---
 const FiEdit = () => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>;
